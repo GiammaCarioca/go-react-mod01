78431a10ba65bdecdf1a200eb51d6a6cec5c8a4b
Thiago Giammattey <giammattey@gmail.com>
2018-08-21 10:30:56 -0300

    DefaultProps & PropTypes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from "react";
 import { render } from "react-dom";
+import PropTypes from "prop-types";
 
 class Button extends Component {
   render() {
@@ -11,6 +12,15 @@ class Button extends Component {
   }
 }
 
+Button.defaultProps = {
+  children: "Enviar"
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.string
+};
+
 class App extends Component {
   handleClick() {
     alert("botão clicado!");
@@ -20,6 +30,7 @@ class App extends Component {
     return (
       <Fragment>
         <h1>Ciao ragazzi</h1>
+        <Button onClick={() => alert("Botão 1")} />
         <Button onClick={this.handleClick}>Clique aqui</Button>
       </Fragment>
     );
