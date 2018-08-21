08311bb0d7dc2892b31e28d67b9646c4ea34fb33
Thiago Giammattey <giammattey@gmail.com>
2018-08-21 12:42:53 -0300

    estilos com SASS no ReactJS

--- src/index.js
+++ src/index.js
@@ -2,40 +2,12 @@ import React, { Component, Fragment } from "react";
 import { render } from "react-dom";
 import Button from "./Button";
 
+import "./style.scss";
+
 class App extends Component {
   state = {
     counter: 0
   };
-
-  // Montagem/atualização
-  static getDerivedStateFromProps(nextProps, prevState) {
-    return { counter: nextProps.initialCounter };
-  }
-
-  // Montagem
-  componentDidMount() {}
-
-  // Atualizações
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextState.counter <= 10;
-  }
-
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    return prevState.counter;
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log(prevState, snapshot);
-  }
-
-  // Desmontagem
-  componentWillUnmount() {}
-
-  // Error
-
-  componentDidCatch(error, info) {
-    console.log("ERRO", error);
-  }
 
   handleClick = () => {
     this.setState({ counter: this.state.counter + 1 });
@@ -44,12 +16,12 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <h1>Ciao ragazzi</h1>
-        <h2>{this.state.counter}</h2>
+        <h1 className="title">Ciao ragazzi</h1>
+        <h2 style={{ color: "#F00" }}>{this.state.counter}</h2>
         <Button onClick={this.handleClick}>Somar</Button>
       </Fragment>
     );
   }
 }
 
-render(<App initialCounter={3} />, document.getElementById("app"));
+render(<App />, document.getElementById("app"));
